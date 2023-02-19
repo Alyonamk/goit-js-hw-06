e26daee73e240ee440f95872ecac57f95841571f
Alyonamk <110933584+Alyonamk@users.noreply.github.com>
2023-02-19 19:24:17 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -4,16 +4,23 @@
 // Для генерування випадкового кольору використовуй функцію getRandomHexColor.
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
 const btnEl = document.querySelector(".change-color");
 console.log(btnEl);
+
 const colorName = document.querySelector(".color");
 const bodyEl = document.body;
 console.log(bodyEl);
 
 btnEl.addEventListener("click", () => {
-  bodyEl.style.backgroundColor = getRandomHexColor();
-  colorName.textContent = getRandomHexColor();
+  const color = getRandomHexColor();
+  bodyEl.style.backgroundColor = color;
+  colorName.textContent = color;
+  console.log(bodyEl);
 
 });
+
